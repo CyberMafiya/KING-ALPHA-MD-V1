d37a244b2de8d52968260446e24f5262beb2c1c9
KING ALPHA <101397009+AlphaNinjaReal@users.noreply.github.com>
2022-07-13 19:44:44 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,9 +12,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94729352830']
-global.premium = ['94729352830']
-global.ownernomer = '94729352830'
+global.owner = ['94741209621']
+global.premium = ['94741209621']
+global.ownernomer = '94741209621'
 global.ownername = 'ШHłТΞ HΛϾКΞЯS'
 global.botname = '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘]'
 global.footer = '</> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴋɪɴɢ ʙᴏᴛ </>️ ▷'
